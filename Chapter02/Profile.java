80620acf94d9ac41031cc6f5045a961b0d5e1b8a
dongHyo <kdhyo98@gmail.com>
2022-01-21 21:42:24 +0900

    refactor : 디렉토리 정리

--- Chapter02/Profile.java
+++
@@ -1,9 +0,0 @@
-public class Profile {
-    public static void main(String[] args) {
-        String name = "Kim Dong Hyo";
-        int age = 25;
-
-        System.out.println("My name is " + name);
-        System.out.println("My age is " + age);
-    }
-}
